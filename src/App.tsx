9899da0529f7f054b0d8a8c943ff8757cb084af7
Simon Lang <simon.lang@lens.org>
2021-12-18 12:59:20 +1000

    Implement grouping

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,92 @@
-import React from "react";
-import "./App.css";
-import data from "./examinations.json";
-const examinations: Array<Examination> = data.examinations;
+import React from 'react'
+import _ from 'lodash'
+import './App.css'
+import classNames from 'classnames'
+import data from './examinations.json'
+
+const images: Array<Image> = []
+data.examinations.forEach((examination: any) => {
+  examination.images.forEach((image: any) => {
+    images.push({
+      ...image,
+      date: examination.date,
+    })
+  })
+})
+
+// TODO: flatten the array so dates is on each. Then a simple groupBy will do the trick
 
 interface Image {
-  eye: string;
-  modality: string;
-  note: string;
-  thumbnail: string;
+  date: string
+  eye: string
+  modality: string
+  note: string
+  thumbnail: string
 }
 
 interface Examination {
-  date: string;
-  images: Array<Image>;
+  date: string
+  images: Array<Image>
+}
+
+function Card(props: any) {
+  const image: Image = props.image
+  return (
+    <div className="col-md-4">
+      <img src={process.env.PUBLIC_URL + image.thumbnail} />
+      <div>Eye: {image.eye}</div>
+      <div>Modality: {image.modality}</div>
+      <div>Note: {image.note}</div>
+    </div>
+  )
 }
 
 function App() {
+  const [group, setGroup] = React.useState<string>('date')
+  const [grouped, setGrouped] = React.useState<any>(_.groupBy(images, group))
+  React.useEffect(() => {
+    setGrouped(_.groupBy(images, group))
+  }, [group])
+  const filterClass = (field: string) => {
+    const active = field === group
+    return classNames({
+      btn: true,
+      'btn-default': !active,
+      'btn-primary': active,
+    })
+  }
   return (
     <div className="App container">
-      <h1>Examinations</h1>
-      <div className="btn-group" hidden>
-        <div className="btn btn-primary">Modality</div>
-        <div className="btn btn-default">Other</div>
+      <div className="btn-group">
+        <div className={filterClass('date')} onClick={() => setGroup('date')}>
+          Date
+        </div>
+        <div
+          className={filterClass('modality')}
+          onClick={() => setGroup('modality')}
+        >
+          Modality
+        </div>
+        <div className={filterClass('eye')} onClick={() => setGroup('eye')}>
+          Eye
+        </div>
       </div>
 
-      {examinations.map((examination: Examination) => {
+      {Object.keys(grouped).map((key: string) => {
         return (
           <div className="row">
             <div className="col-12">
-              <h2>{examination.date}</h2>
+              <h2>{key}</h2>
             </div>
-            {examination.images.map((image: Image) => {
-              return (
-                <div className="col-md-4">
-                  <img src={process.env.PUBLIC_URL + image.thumbnail} />
-                  <div>Eye: {image.eye}</div>
-                  <div>Modality: {image.modality}</div>
-                  <div>Note: {image.note}</div>
-                </div>
-              );
-            })}
+            {grouped[key].map((image: Image) => (
+              <Card image={image} />
+            ))}
           </div>
-        );
+        )
       })}
-      <pre>{JSON.stringify(examinations, null, 2)}</pre>
+      <pre>{JSON.stringify(grouped, null, 2)}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
